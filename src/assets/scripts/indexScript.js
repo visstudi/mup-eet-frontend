dbddba5213198113742e03093347399fa87bf6d0
visstudi <dungeongachi6hotloads@gmail.com>
2026-03-18 00:30:59 +0400

    Make primary main page layout

--- src/assets/scripts/indexScript.js
+++ src/assets/scripts/indexScript.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const tabButtons = document.querySelectorAll(".tab button");
+
+tabButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.parentElement.classList.contains("active")) {
+      button.nextElementSibling.style.maxHeight = `0px`;
+    } else {
+      button.nextElementSibling.style.maxHeight = `${button.nextElementSibling.scrollHeight}px`;
+    }
+    button.parentElement.classList.toggle("active");
+  });
+});
