79da773678d637b9750d5865f00d6af8d68210bd
visstudi <dungeongachi6hotloads@gmail.com>
2026-03-30 02:58:22 +0400

    Add dark theme

--- src/assets/scripts/dark-mode.js
+++ src/assets/scripts/dark-mode.js
@@ -1,29 +1,46 @@
 "use strict";
 
 const root = document.getElementsByTagName("html")[0];
+
 const logo = document.getElementsByClassName("logo")[1];
-const themeSwitchButton = document.getElementById("dark-mode-button");
+
 const themeSwitchIcon = document.getElementById("dark-mode-icon");
+
 const userPrefersDarkMode =
   window.matchMedia &&
   window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-function toggleTheme() {
-  if (root.getAttribute("theme") === "dark") {
-    root.setAttribute("theme", "light");
-    logo.setAttribute("src", "../assets/icons/logo-light.svg");
-    themeSwitchIcon.setAttribute("src", "../assets/icons/darkmode.svg");
-  } else {
-    root.setAttribute("theme", "dark");
-    logo.setAttribute("src", "../assets/icons/logo-dark.svg");
-    themeSwitchIcon.setAttribute("src", "../assets/icons/lightmode.svg");
-  }
+function enableTheme(theme) {
+  root.setAttribute("theme", theme);
+  logo.setAttribute("src", `../assets/icons/logo-${theme}.svg`);
+  themeSwitchIcon.setAttribute(
+    "src",
+    `../assets/icons/switch-mode-${theme}.svg`,
+  );
+  localStorage.setItem("selected-theme", theme);
 }
 
-themeSwitchButton.addEventListener("click", (event) => {
-  toggleTheme();
-});
+function toggleTheme() {
+  root.getAttribute("theme") === "dark"
+    ? enableTheme("light")
+    : enableTheme("dark");
+}
+
+document
+  .getElementById("dark-mode-button")
+  .addEventListener("click", (event) => {
+    toggleTheme();
+  });
 
 window
   .matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener("change", (event) => {});
+  .addEventListener("change", (event) => {
+    enableTheme(event.matches ? "dark" : "light");
+  });
+
+/* Prefer selected-theme saved in localStorage over user's preferred theme */
+if (userPrefersDarkMode && localStorage.getItem("selected-theme") === null) {
+  enableTheme("dark");
+} else {
+  enableTheme(localStorage.getItem("selected-theme"));
+}
