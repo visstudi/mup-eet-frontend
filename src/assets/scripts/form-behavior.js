0f06993301fd0205116f24c9450e1ecd1ffbf41d
visstudi <dungeongachi6hotloads@gmail.com>
2026-04-07 08:10:23 +0400

    Demo https cors

--- src/assets/scripts/form-behavior.js
+++ src/assets/scripts/form-behavior.js
@@ -57,7 +57,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Валидация формы
-const API_BASE_URL = "http://5.3.250.90:5153";
+const API_BASE_URL =
+  "https://cors-anywhere.herokuapp.com/http://5.3.250.90:5153";
 
 const feedbackForm = document.querySelector("form");
 
--- src/assets/scripts/routes.js
+++ src/assets/scripts/routes.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const API_BASE_URL = "http://5.3.250.90:5153";
+  const API_BASE_URL =
+    "https://cors-anywhere.herokuapp.com/http://5.3.250.90:5153";
   const container = document.querySelector(".tabs-container");
 
   const hexToRgb = (hex) => {
